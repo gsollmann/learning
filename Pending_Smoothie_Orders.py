2060238503f753f6071f19eb77e8f13c0ef3dc64
gsollmann <gsollmann@users.noreply.github.com>
2025-12-26 13:43:34 -0500

    Update Pending_Smoothie_Orders.py

--- Pending_Smoothie_Orders.py
+++ Pending_Smoothie_Orders.py
@@ -7,7 +7,8 @@ from snowflake.snowpark.functions import col, when_matched
 st.title(f"Pending Smoothie Orders :cup_with_straw:")
 st.write("""Orders that need to be filled.""")
 
-session = get_active_session()
+cnx = st.connection("snowflake")
+session = cnx.session()
 
 my_dataframe = session.table("smoothies.public.orders").filter(col("ORDER_FILLED")==0).collect()
 if my_dataframe:
